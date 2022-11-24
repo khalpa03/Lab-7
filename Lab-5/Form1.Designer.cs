98244f5d82cf52a6b430690e08590923c0e73a8e
Khalpa03 <khalmanov2003@mail.ru>
2022-11-24 20:14:01 +0800

    first

---
+++ Lab-5/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Lab_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(324, 71);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Создать Word документ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите текст для Word";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(321, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(28, 209);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(321, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(266, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Введите числа для Excel через пробел";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(28, 237);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(324, 71);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Создать Excel документ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 399);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Работа с Word и Excel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Lab-5/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace Lab_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length!=0)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Title = "Создание word документа";
+                sfd.FileName = "Word файл";
+                if (sfd.ShowDialog() == DialogResult.Cancel)
+                    return;
+                string filename = sfd.FileName;
+                var app = new Word.Application();
+                app.Visible = true;
+                var doc = app.Documents.Add();
+                var r = doc.Range();
+                r.Bold = 1;
+                r.Text = textBox1.Text;
+                doc.SaveAs(filename);
+            }
+            else
+            {
+                MessageBox.Show("Заполните строку!!!");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] line= textBox2.Text.Split(' ');
+
+            if (textBox2.Text.Length != 0)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Title = "Создание Excel документа";
+                sfd.FileName = "Excel файл";
+                sfd.Filter = "xls files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+                if (sfd.ShowDialog() == DialogResult.Cancel)
+                    return;
+                string filename = sfd.FileName;
+                var ex = new Excel.Application();
+                ex.Visible = true;
+                ex.SheetsInNewWorkbook = 2;
+                var workbook = ex.Workbooks.Add(Type.Missing);
+                var sheet = (Excel.Worksheet)ex.Worksheets.get_Item(1);
+
+                for (int j = 1; j <= line.Length; j++)
+                    sheet.Cells[1, j] = String.Format(line[j - 1], 1, j);
+                ex.Application.ActiveWorkbook.SaveAs(filename, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+                
+            }
+            else
+            {
+                MessageBox.Show("Заполните строку числами!!!");
+            }
+        }
+    }
+}
